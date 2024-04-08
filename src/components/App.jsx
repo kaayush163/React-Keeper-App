39e841fc74ac649cf5a4ed737bd5764079109455
Aayush <88710500+kaayush163@users.noreply.github.com>
2024-04-08 23:22:11 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer";
+import Note from "./Note";
+function App() {
+  return (
+    <div>
+      {/* <h1> Hello App</h1> */}
+      <Header/>
+      <Note/>
+      <Footer/>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return ( 
+    <div>
+        <h1>THis is the title</h1>
+        <p> THis is the content</p>
+    </div>
+    );
+}
+
+export default Note;
